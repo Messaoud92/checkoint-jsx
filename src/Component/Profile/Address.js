9c2715ff9ff4ac7a53efad6346c3d3e3edb2e274
Messaoud92 <messaoud.souid@gmail.com>
2022-11-14 12:50:30 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Address() {
+  return (
+    <div style={{ marginTop: -20 , color: "white", fontSize: 60 }}>Gabes Hackerspace</div>
+  )
+}
+
+export default Address
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../fulnam.css"
+
+function FullName() {
+  return (
+    <div>
+        <span className='pnom' >Messaoud SOUID</span>
+       
+        <p> full stack/embedded system developer</p>
+    </div>
+  )
+}
+
+export default FullName
